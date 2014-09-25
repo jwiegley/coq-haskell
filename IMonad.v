8bde1cf481b91fd6bb0e30cdb5cd8e69e13a722e
John Wiegley <johnw@newartisans.com>
2014-09-25 00:02:08 -0500

    Snapshot

--- IMonad.v
+++ IMonad.v
@@ -6,24 +6,25 @@ Section Indexed.
 
 Variable A : Type.
 
-Definition inat (P Q : A -> Type) := forall x : A, P x -> Q x.
+Definition inat (P Q : A → Type) := ∀ x : A, P x → Q x.
 
-Notation "s :-> t" := (inat s t) (at level 28).
+Notation "s :→ t" := (inat s t) (at level 28).
 
-Definition idx_id {X} : X :-> X.
-  unfold ":->". intros. apply X0.
+Definition idx_id {X} : X :→ X.
+  unfold ":→". intros.
+  apply X0.
 Defined.
 
-Definition idx_compose {X Y Z} : (Y :-> Z) -> (X :-> Y) -> (X :-> Z).
-  unfold ":->". intros.
+Definition idx_compose {X Y Z} : (Y :→ Z) → (X :→ Y) → (X :→ Z).
+  unfold ":→". intros.
   apply X0. apply X1. apply X2.
 Defined.
 
 End Indexed.
 
-Class Category {k} (cat : k -> k -> Type) := {
+Class Category {k} (cat : k → k → Type) := {
     id : ∀ {a}, cat a a;
-    compose : ∀ {a b c}, cat b c -> cat a b -> cat a c
+    compose : ∀ {a b c}, cat b c → cat a b → cat a c
 }.
 
 Program Instance Index_Category {A} : Category (@inat A) := {
@@ -33,32 +34,28 @@ Program Instance Index_Category {A} : Category (@inat A) := {
 
 Arguments inat [A] _ _.
 
-Notation "s :-> t" := (inat s t) (at level 100).
+Notation "s :→ t" := (inat s t) (at level 100).
 Notation "f ∘ g" := (compose f g) (at level 40, left associativity).
 
-Class IFunctor {I O} (F : (I -> Type) -> (O -> Type)) := {
+Class IFunctor {I O} (F : (I → Type) → (O → Type)) := {
     iobj := F;
-    imap : forall {X Y}, (X :-> Y) -> (F X :-> F Y);
+    imap : ∀ {X Y}, (X :→ Y) → (F X :→ F Y);
 
-    ifun_identity : forall {X}, imap (id (a := X)) = id;
-    ifun_composition : forall {X Y Z} (f : Y :-> Z) (g : X :-> Y),
+    ifun_identity : ∀ {X}, imap (id (a := X)) = id;
+    ifun_composition : ∀ {X Y Z} (f : Y :→ Z) (g : X :→ Y),
       imap f ∘ imap g = imap (f ∘ g)
 }.
 
-Arguments imap             [I O F IFunctor X Y] f x _.
-Arguments ifun_identity    [I O F IFunctor X].
-Arguments ifun_composition [I O F IFunctor X Y Z] f g.
-
 Notation "imap[ M ]  f" := (@imap _ _ M _ _ _ f) (at level 28).
 Notation "imap[ M N ]  f" :=
-  (@imap _ _ (fun X => M (N X)) _ _ _ f) (at level 26).
+  (@imap _ _ (λ X, M (N X)) _ _ _ f) (at level 26).
 Notation "imap[ M N O ]  f" :=
-  (@imap _ _ (fun X => M (N (O X))) _ _ _ f) (at level 24).
+  (@imap _ _ (λ X, M (N (O X))) _ _ _ f) (at level 24).
 
 Coercion iobj : IFunctor >-> Funclass.
 
 Lemma ifun_irrelevance `(F : IFunctor)
-  : ∀ (f g : ∀ {X Y}, (X :-> Y) → (F X :-> F Y))
+  : ∀ (f g : ∀ {X Y}, (X :→ Y) → (F X :→ F Y))
       i i' c c',
   @f = @g →
   {| imap := @f
@@ -73,20 +70,20 @@ Proof.
   apply proof_irrelevance.
 Qed.
 
-Inductive Path {I : Type} : ((I * I) → Type) -> (I * I) → Type :=
-  | Stop g : forall {i : I}, Path g (i,i)
-  | Link g : forall {i j k : I}, g (i,j) → Path g (j,k) → Path g (i,k).
+Inductive Path {I : Type} (g : (I * I) → Type) : (I * I) → Type :=
+  | Stop : ∀ {i : I}, Path g (i,i)
+  | Link : ∀ {i j k : I}, g (i,j) → Path g (j,k) → Path g (i,k).
 
 Arguments Stop [I g i].
 Arguments Link [I g i j k] _ _.
 
 Notation "f :-: g" := (Link f g) (at level 50).
 
-Definition path_imap {I} {k h : (I * I) → Type} (f : k :-> h)
+Definition path_imap {I} {k h : (I * I) → Type} (f : k :→ h)
   {x : I * I} (p : Path k x) : Path h x.
-  induction p as [| ? ? ? ? r rs].
+  induction p.
     apply Stop.
-  apply (Link (f _ r) (IHrs f)).
+  apply (Link (f _ g) IHp).
 Defined.
 
 Program Instance Path_IFunctor {I} : @IFunctor (I * I) (I * I) (@Path I) := {
@@ -107,27 +104,57 @@ Obligation 2.
   f_equal. apply IHp.
 Qed.
 
-Inductive IAssign {x : Type} : Type -> x -> x -> Type :=
-  | V a k : IAssign a k k.
+Inductive IAssign {I : Type} : Type → I → I → Type :=
+  | V a k : a → IAssign a k k.
 
 Infix "::=" := IAssign (at level 50).
 
-Lemma IAssign_unique : forall a (x : a) t (k h : (a ::= x) t), k = h.
-Proof.
-Admitted.
+Definition list_to_Path : ∀ {a}, list a → Path (a ::= (tt, tt)) (tt, tt).
+  intros.
+  induction X.
+    apply Stop.
+  apply Link with (j := tt).
+  apply V. apply a0.
+  apply IHX.
+Defined.
 
-Definition List a := Path (a ::= (unit,unit)) (unit,unit).
+Definition Path_to_list : ∀ {a}, Path (a ::= (tt, tt)) (tt, tt) → list a.
+  intros.
+  induction X.
+    apply nil.
+  apply cons.
+  inversion g. apply X0.
+  apply IHX.
+Defined.
 
-Program Instance KeyIndex_Iso : forall (a : Type) (t : a -> Type) (k : a),
-  ((a ::= k) :-> t) ≅ t k.
+Program Instance Path_List_Iso
+  : ∀ a, list a ≅ Path (a ::= (tt, tt)) (tt, tt) := {
+    to   := list_to_Path;
+    from := Path_to_list
+}.
 Obligation 1.
-  unfold ":->" in X.
+  extensionality l.
+  induction l. reflexivity.
+  compute. f_equal.
+  apply IHl.
+Qed.
+Obligation 2.
+  extensionality p.
+  inversion p.
+    auto.
+Admitted.                       (* need to do induction on p! *)
+
+Program Instance KeyIndex_Iso : ∀ (a I : Type) (t : I → Type) (k : I),
+  ((a ::= k) :→ t) ≅ (a → t k).
+Obligation 1.
+  unfold ":→" in X.
   apply X. constructor.
+  apply X0.
 Defined.
 Obligation 2.
-  unfold ":->". intros.
+  unfold ":→". intros.
   inversion X0; subst.
-  apply X.
+  apply X. apply X1.
 Defined.
 Obligation 3.
   unfold KeyIndex_Iso_obligation_1.
@@ -138,5 +165,183 @@ Obligation 3.
   extensionality H.
   inversion H; subst.
   destruct H. simpl.
-  crush.
+  unfold eq_rect_r. simpl.
+  reflexivity.
 Qed.
+
+Reserved Notation "c ?>>= f" (at level 25, left associativity).
+
+Class IMonad {I} (m : (I → Type) → (I → Type)) `{IFunctor I I m} := {
+    is_ifunctor :> IFunctor m;
+
+    iskip   : ∀ {p}, p :→ m p;
+    iextend : ∀ {p q}, (p :→ m q) → (m p :→ m q)
+      where "c ?>>= f" := (iextend f _ c);
+
+    imonad_left_id : ∀ (p q : I → Type) (f : p :→ m q) (i : I) (a : p i),
+      iskip i a ?>>= f = f i a;
+    imonad_right_id : ∀ (p q : I → Type) (f : p :→ m q) (i : I) (m : m p i),
+      m ?>>= iskip = m;
+    imonad_assoc : ∀ (p q r : I → Type)
+      (f : p :→ m q) (g : q :→ m r) (i : I) (m : m p i),
+      (m ?>>= f) ?>>= g = m ?>>= (λ x a, f x a ?>>= g)
+}.
+
+Coercion is_ifunctor : IMonad >-> IFunctor.
+
+Definition iseq `{m : IMonad} {p q r} (f : p :→ m q) (g : q :→ m r)
+  : p :→ m r := iextend g ∘ f.
+
+Program Instance Path_IMonad {I} : IMonad (@Path I).
+Obligation 1.
+  unfold ":→". intros.
+  destruct x.
+  apply Link with (j := i0).
+    apply X.
+  apply Stop.
+Defined.
+Obligation 2.
+  unfold ":→". intros x P.
+  destruct x as [i j].
+  apply (imap X) in P.
+  inversion P; subst.
+    apply Stop.
+Admitted.
+Obligation 3.
+Admitted.
+Obligation 4.
+Admitted.
+Obligation 5.
+Admitted.
+
+(*************************************************************************)
+
+Inductive IState {s : Type} (P : relation s) (a : s → Type) : s → Type :=
+  | St st : (forall st', P st st' → { st'' : s & a st'' & P st' st'' }) → IState P a st.
+
+Arguments St [s P a] st _.
+
+Definition runIState {s : Type} {P : relation s} {a : s → Type} (st : s) (x : IState P a st)
+  : (forall st', P st st' → { st'' : s & a st'' & P st' st'' }) := match x with St _ p => p end.
+
+Definition IState_fmap {s : Type} (P : relation s) {a b : s → Type}
+  (f : a :→ b) : IState P a :→ IState P b := λ (st : s) x,
+  St st (λ st' H, let p := runIState st x st' H in
+                  match p with
+                  | existT2 st'' x H' =>
+                    existT2 _ _ st'' (f st'' x) H'
+                  end).
+
+Program Instance IState_IFunctor {s : Type} (P : relation s) : IFunctor (@IState s P) := {
+    imap := @IState_fmap s P
+}.
+Obligation 1.
+  compute.
+  extensionality st.
+  extensionality x.
+  destruct x.
+  f_equal.
+  extensionality st'.
+  extensionality H.
+  destruct (s0 st').
+  reflexivity.
+Qed.
+Obligation 2.
+  compute.
+  extensionality st.
+  extensionality x.
+  destruct x.
+  f_equal.
+  extensionality st'.
+  extensionality H.
+  destruct (s0 st').
+  reflexivity.
+Qed.
+
+Program Instance IState_IMonad {s : Type} (P : relation s) : IMonad (@IState s P) := {
+    iskip := fun a st x =>
+      St st (λ st' H, existT2 a (P st') st' x H)(* ; *)
+    (* iextend := fun P Q f st x => *)
+    (*   St st (λ st', let (a, st'') := runIState st x st' in *)
+    (*                 runIState st' (f st a) st'') *)
+}.
+Obligation 1.
+  compute. destruct (f i a). reflexivity.
+Defined.
+Obligation 2.
+  compute. destruct m. destruct p0.
+Obligation 1.
+  compute. intros.
+  constructor.
+  split; assumption.
+Defined.
+Obligation 2.
+  compute. intros.
+  apply (imap X) in X0.
+  destruct X0. destruct p0.
+  assumption.
+Defined.
+Obligation 4.
+  destruct m. destruct p0.
+  compute.
+  f_equal. f_equal.
+
+Section IState.
+
+Variable   s : Type.
+Hypothesis P : relation s.
+Context  `(e : PreOrder s P).
+
+Record StateP (before : s) (a : s → Type) := {
+    after  : s;
+    result : a after;
+    holds  : P before after
+}.
+
+Arguments after [before] [a] _.
+Arguments result [before] [a] _.
+Arguments holds [before] [a] _.
+
+(** The [IState] monad requires that a given equivalence relation hold
+    between state transitions. *)
+Inductive IState (st : s) (a : s → Type) : Type :=
+  | St : StateP st a → IState st a.
+
+Arguments St st [a] _.
+
+Definition runIState (st : s) {a : s → Type} (v : IState st a)
+  : StateP st a := match v with St f => f end.
+
+Definition IState_fmap (st : s) {a b : s → Type}
+  (f : forall st, a st → b st) (x : IState st a) : IState st b :=
+  St st (let sp := runIState st x in
+          {| after  := after sp
+           ; result := f (after sp) (result sp)
+           ; holds  := holds sp
+           |}).
+
+Hint Unfold IState_fmap.
+
+Ltac IState_auto :=
+  intros; simpl;
+  repeat (
+    autounfold; unfold id, compose; simpl;
+    f_equal; try apply proof_irrelevance; auto;
+    match goal with
+    | [ |- (fun _ : IState _ => _) = (fun _ : IState _ => _) ] =>
+        extensionality sx
+    | [ |- (fun _ : s => _) = _ ] =>
+        extensionality st
+    | [ st : s, sp : forall st : s, StateP st _ |- _ ] =>
+        destruct (sp st)
+    | [ sx : IState _ |- _ ] => destruct sx as [sp]
+    | [ |- St _ = St _ ] => f_equal
+    end).
+
+Obligation Tactic := IState_auto.
+
+End IState.
+
+Program Instance IState_IFunctor : IFunctor IState := {
+    fmap := IState_fmap
+}.
