82797e4deacc18a7996acdf229814eec33a0fd57
John Wiegley <johnw@newartisans.com>
2015-06-08 00:00:35 -0500

    Move some code around

--- src/Control/Pipes.v
+++ src/Control/Pipes.v
@@ -332,6 +332,36 @@ Definition stream `(co : Proxy a' a b' b m r) : CoProxy a' a b' b m r :=
     end in
   go co.
 
+
+(*
+Inductive pushR_ev {a' a b' b c' c m r}
+  (p0 : CoProxy a' a b' b m r) (fb : b -> CoProxy b' b c' c m r) : Prop :=
+  | ev_pushR_req : forall aa' fa, p0 = CoRequest aa' fa -> pushR_ev p0 fb
+  | ev_pushR_res : forall bb  fb',
+      p0 = CoRespond bb fb' -> pullR_ev fb' (fb bb) -> pushR_ev p0 fb
+
+with pullR_ev {a' a b' b c' c m r}
+  (fb' : b' -> Proxy a' a b' b m r) (p0 : Proxy b' b c' c m r) : Prop :=
+  | ev_pullR_req : forall aa' fa, p0 = CoRequest aa' fa -> pullR_ev fb' p0.
+
+CoFixpoint pushR `{Monad m} {a' a b' b c' c r} (p0 : CoProxy a' a b' b m r)
+  (fb : b -> CoProxy b' b c' c m r) {struct p0} : CoProxy a' a c' c m r :=
+  let cofix go p := match p with
+    | CoRequest a' fa  => CoRequest a' (go \o fa)
+    | CoRespond b  fb' =>
+        let cofix go' p := match p with
+          | CoRequest b' fb  => go (fb' b')
+          | CoRespond c  fc' => CoRespond c (go' \o fc')
+          | CoM _     f  t   => CoM (go' \o f) t
+          | CoPure       a   => CoPure a
+          end in
+        go' (fb b)
+    | CoM _     f  t   => CoM (go \o f) t
+    | CoPure       a   => CoPure a
+    end in
+  go p0.
+*)
+
 (*
 Program Instance Push_Category
   (n : nat) (_ : n > 0) {r} (dflt : r) `{MonadLaws m} :
@@ -394,6 +424,70 @@ Theorem zero_law
 
 Theorem involution
 
+*)
+
+Definition SProxy (a' a b' b : Type) (m : Type -> Type) (r : Type) : Type :=
+  forall s : Type,
+       (a' -> (a  -> s) -> s)           (* SRequest *)
+    -> (b  -> (b' -> s) -> s)           (* SRespond *)
+    -> (forall x, (x -> s) -> m x -> s) (* SM *)
+    -> (r -> s)                         (* SPure *)
+    -> s.
+
+Definition toProxy `(s : SProxy a' a b' b m r) : Proxy a' a b' b m r :=
+  s _ Request Respond (fun _ => M) Pure.
+
+Fixpoint fromProxy `(p : Proxy a' a b' b m r) : SProxy a' a b' b m r :=
+  fun _ req res mon k =>
+    match p with
+    | Request a' fa  => req a' (fun a  => fromProxy (fa  a)  _ req res mon k)
+    | Respond b  fb' => res b  (fun b' => fromProxy (fb' b') _ req res mon k)
+    | M _     g  h   => mon _  (fun x  => fromProxy (g x) _ req res mon k) h
+    | Pure    x      => k x
+    end.
+
+Lemma SProxy_to_from : forall `(x : Proxy a' a b' b m r),
+  toProxy (fromProxy x) = x.
+Proof.
+  move=> a' a b' b m r.
+  reduce_proxy IHx
+    (rewrite /toProxy;
+     first [ congr (Request _)
+           | congr (Respond _)
+           | move=> m0; congr (M _)
+           | congr (Pure _) ]).
+Qed.
+
+(* Definition pushS `{Monad m} {a' a r} : a -> SProxy a' a a' a m r := *)
+(*   fun x _ req res mon k => *)
+(*     res x (fun a' => req a' (fun a => pushS a _ req res mon k)). *)
+
+Axiom elim : forall `(f : a -> (b -> s) -> s) (x : a) (y : s),
+  f x (const y) = y.
+
+Axiom flip_elim : forall `(f : (b -> s) -> a -> s) (x : a) (y : s),
+  f (const y) x = y.
+
+(*
+Lemma SProxy_from_to : forall `(x : SProxy a' a b' b m r),
+  fromProxy (toProxy x) = x.
+Proof.
+  move=> a' a b' b m r x.
+  extensionality s.
+  extensionality req.
+  extensionality res.
+  extensionality mon.
+  extensionality k.
+  (* elim E: (toProxy x) => [? ? IHu|? ? IHu|? ? IHu| ?]. *)
+  (* admit. admit. admit. *)
+  (* rewrite /fromProxy /=. *)
+  move: (toProxy x).
+  reduce_proxy IHx
+    (rewrite /fromProxy /=;
+     try (move/functional_extensionality in IHx;
+          try move=> m0;
+          rewrite IHx ?elim ?flip_elim)).
+Admitted.
 *)
 
 Section GeneralTheorems.
