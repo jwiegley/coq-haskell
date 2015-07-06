23cfb9e94b826ef920abf9b74ea6aca5f33d6431
John Wiegley <johnw@newartisans.com>
2015-07-06 00:32:59 -0700

    Add decide helper function to Ssr.v

--- src/Ssr.v
+++ src/Ssr.v
@@ -8,3 +8,17 @@ Require Export Ssreflect.fintype.
 
 Lemma and_swap : forall x y z, [&& x, y & z] = [&& y, x & z].
 Proof. by case; case; case. Qed.
+
+Definition decide {T : Type} (H : bool)
+  (kt : (H = true)  -> T)
+  (kf : (H = false) -> T) : T :=
+  (fun (if_true  : (fun b : bool => protect_term (H = b) -> T) true)
+       (if_false : (fun b : bool => protect_term (H = b) -> T) false) =>
+    if H as b return ((fun b0 : bool => protect_term (H = b0) -> T) b)
+    then if_true
+    else if_false)
+  (fun (E : H = true)  => kt E)
+  (fun (E : H = false) => kf E)
+  (erefl H).
+
+Arguments decide {T} H kt kf.
