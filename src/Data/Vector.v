19c6318b35eca1a1df7b5c626781e560ab241776
John Wiegley <johnw@newartisans.com>
2015-10-15 14:03:52 -0700

    Add vtranspose

--- src/Data/Vector.v
+++ src/Data/Vector.v
@@ -403,4 +403,22 @@ Example vmap_check :
     == (vcons 3 (vcons 4 (vcons 5 (vcons 6 (vsing 7))))).
 Proof. reflexivity. Qed.
 
+Definition vtranspose {m n : nat} a : Vec (Vec a n) m -> Vec (Vec a m) n.
+Proof.
+  move=> v.
+  elim: m => /= [|m IHm] in v *.
+    by elim: n.
+  inv v.
+  elim: n => //= [n IHn] in IHm X X0 *.
+  split.
+    split.
+      by case: X.
+    by case: IHm.
+  apply: IHn.
+    move=> Y.
+      by case: IHm.
+    by case: X.
+  exact: vmap snd X0.
+Defined.
+
 End VectorSpec.
