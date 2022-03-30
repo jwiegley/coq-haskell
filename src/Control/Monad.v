44d3c771ac421f1fa1e1d689491a60edfce0ddc5
John Wiegley <johnw@newartisans.com>
2022-03-30 15:26:36 -0700

    Clarify more arguments

--- src/Control/Monad.v
+++ src/Control/Monad.v
@@ -106,7 +106,7 @@ Fixpoint flatten `(xs : list (list A)) : list A :=
 Definition concatMapM `{Applicative m} {A B}
   (f : A -> m (list B)) (l : list A) : m (list B) :=
   let xs : m (list (list B)) := mapM (m:=m) f l in
-  fmap (f:=m) (Functor:=is_functor) flatten xs.
+  fmap (f:=m) (b:=list B) (Functor:=is_functor) flatten xs.
 
 Fixpoint replicateM_ `{Monad m} (n : nat) (x : m unit) : m unit :=
   match n with
